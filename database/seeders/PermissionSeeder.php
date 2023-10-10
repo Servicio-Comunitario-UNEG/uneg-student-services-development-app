8328af03f9b42d87521ba3fb347c77f4e02f4e79
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-10-10 13:57:08 -0400

    feat: create roles

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		// Reset cached roles and permissions.
+		app()[
+			\Spatie\Permission\PermissionRegistrar::class
+		]->forgetCachedPermissions();
+
+		// Create the roles.
+		$roles = ["coordinator", "representative"];
+
+		foreach ($roles as $role) {
+			Role::create(["name" => $role]);
+		}
+	}
+}
