819382eeb9fef222aca8616f921c9861a6564c98
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2024-01-20 16:45:32 -0400

    fix: use login as base page

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::get("/", function () {
-	return Inertia::render("Welcome", [
-		"canLogin" => Route::has("login"),
-		"canRegister" => Route::has("register"),
-		"laravelVersion" => Application::VERSION,
-		"phpVersion" => PHP_VERSION,
-	]);
-});
+Route::get("/", fn() => redirect("/login"));
 
 Route::get("/home", function () {
 	return Inertia::render("Home");
